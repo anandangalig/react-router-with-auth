dc197fbd0c6cb5227752c90f216a8d381646e3d9
Anand Angalig <anandangalig@gmail.com>
2019-03-28 15:21:51 -0400

    add static Header component to render on all routes

---
+++ client/src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+
+const Header = () => {
+    return (
+       <div className='ui secondary pointing menu'>
+            <Link to='/'>STREAM APP</Link>
+            <div className='right menu'>
+                <Link to='/'>All Streams</Link>
+            </div>
+        </div>
+    );
+}
+
+export default Header;
