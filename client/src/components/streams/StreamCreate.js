cd7758248df2a48db343fdfc4c55c086ab711b12
Anand Angalig <anandangalig@gmail.com>
2019-05-23 12:06:13 -0400

    create StreamForm to be reused by StreamCreate and StreamEdit; hooked up StreamCreate to it

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,70 +1,27 @@
 import React from 'react';
-import { Field, reduxForm } from 'redux-form';
 import { connect } from 'react-redux';
 import { createStream } from '../../actions';
+import StreamForm from './StreamForm';
 
 class StreamCreate extends React.Component {
-  renderErrorMessage({ error, touched }) {
-    if (touched && error) {
-      return (
-        <div className="ui error message">
-          <div className="header">{error}</div>
-        </div>
-      );
-    }
-  }
-  renderInput = fieldProps => {
-    //fieldProps has extra props coming from Field(redux-form)
-    const conditionalClassNames = `field ${
-      fieldProps.meta.error && fieldProps.meta.touched ? 'error' : ''
-    }`;
-    return (
-      <div className={conditionalClassNames}>
-        <label>{fieldProps.label}</label>
-        <input {...fieldProps.input} autoComplete="off" />
-        {this.renderErrorMessage(fieldProps.meta)}
-      </div>
-    );
-  };
-
-  mySubmit = formValues => {
+  onSubmit = formValues => {
     this.props.createStream(formValues);
   };
 
   render() {
     return (
-      <form className="ui form error" onSubmit={this.props.handleSubmit(this.mySubmit)}>
-        <Field name="title" component={this.renderInput} label="Enter title" />
-        <Field name="description" component={this.renderInput} label="Enter description" />
-        <button className="ui button primary">submit</button>
-      </form>
+      <div>
+        <h3>Create a Stream</h3>
+        <StreamForm onSubmit={this.onSubmit} />
+      </div>
     );
   }
 }
 
-const validate = formValues => {
-  // when hooked up, this will run with each render AND form interaction on the DOM
-  // if returned empty, redux-form will take it as no errors during validation
-  // redux-form will try and match the errors properties to <Field /> name prop. If any matches, it will take the error message and pass it to component prop of the <Field /> in the meta object
-  const errors = {};
-  if (!formValues.title) {
-    errors.title = 'Enter a title';
-  }
-  if (!formValues.description) {
-    errors.description = 'Enter a description';
-  }
-  return errors;
-};
-
-const formWrapped = reduxForm({
-  form: 'streamCreate',
-  validate: validate,
-})(StreamCreate);
-
 export default connect(
   null,
   { createStream },
-)(formWrapped);
+)(StreamCreate);
 /*
 NOTES:
 Field element does not create the field element on its own. Instead it connects this component to the Redux Store. It needs a component prop that returns the actual field, and connect the onChange and value of the input field
---
+++ client/src/components/streams/StreamForm.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import { Field, reduxForm } from 'redux-form';
+
+class StreamForm extends React.Component {
+  renderErrorMessage({ error, touched }) {
+    if (touched && error) {
+      return (
+        <div className="ui error message">
+          <div className="header">{error}</div>
+        </div>
+      );
+    }
+  }
+  renderInput = fieldProps => {
+    //fieldProps has extra props coming from Field(redux-form)
+    const conditionalClassNames = `field ${
+      fieldProps.meta.error && fieldProps.meta.touched ? 'error' : ''
+    }`;
+    return (
+      <div className={conditionalClassNames}>
+        <label>{fieldProps.label}</label>
+        <input {...fieldProps.input} autoComplete="off" />
+        {this.renderErrorMessage(fieldProps.meta)}
+      </div>
+    );
+  };
+
+  mySubmit = formValues => {
+    this.props.onSubmit(formValues);
+  };
+
+  render() {
+    console.log(this.props);
+
+    return (
+      <form className="ui form error" onSubmit={this.props.handleSubmit(this.mySubmit)}>
+        <Field name="title" component={this.renderInput} label="Enter title" />
+        <Field name="description" component={this.renderInput} label="Enter description" />
+        <button className="ui button primary">submit</button>
+      </form>
+    );
+  }
+}
+
+const validate = formValues => {
+  // when hooked up, this will run with each render AND form interaction on the DOM
+  // if returned empty, redux-form will take it as no errors during validation
+  // redux-form will try and match the errors properties to <Field /> name prop. If any matches, it will take the error message and pass it to component prop of the <Field /> in the meta object
+  const errors = {};
+  if (!formValues.title) {
+    errors.title = 'Enter a title';
+  }
+  if (!formValues.description) {
+    errors.description = 'Enter a description';
+  }
+  return errors;
+};
+
+export default reduxForm({
+  form: 'streamForm',
+  validate: validate,
+})(StreamForm);
+/*
+NOTES:
+Field element does not create the field element on its own. Instead it connects this component to the Redux Store. It needs a component prop that returns the actual field, and connect the onChange and value of the input field
+*/
